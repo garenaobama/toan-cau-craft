723eec1ccf6dd2103004daaa228f3a478cb3ec83
PhuongThan25503 <142188056+PhuongThan25503@users.noreply.github.com>
2024-08-23 00:27:55 +0700

    update: animate home

--- toan-cau-craft/src/components/CategoryCard/index.tsx
+++ toan-cau-craft/src/components/CategoryCard/index.tsx
@@ -13,26 +13,31 @@ export const CategoryCard = ({
   className?: string;
 }) => {
   return (
-    <div className={twMerge("relative h-96 overflow-hidden", className)}>
-      <Image
-        className="absolute"
-        fill
-        objectFit="cover"
-        src={src}
-        alt="category_image"
-      />
-      <div
-        style={{
-          background:
-            "linear-gradient(90deg, rgba(0,0,0,0.7455357142857143) 0%, rgba(255,255,255,0.1516981792717087) 100%);",
-        }}
-        className="w-full h-full absolute"
-      ></div>
-      <div className="absolute p-5 w-full h-full">
-        <div className="border w-full h-full flex items-end border-themeWhite p-6">
-          <h4 className={twMerge(cormorantRegular.className, "text-themeWhite text-4xl")}>{title}</h4>
+    <button>
+      <div className={twMerge("relative h-96 overflow-hidden", className)}>
+        <Image
+          className="absolute"
+          fill
+          objectFit="cover"
+          src={src}
+          alt="category_image"
+        />
+        <div
+          className="w-full h-full absolute bg-custom-gradient"
+        ></div>
+        <div className="absolute p-5 w-full h-full">
+          <div className="border w-full h-full flex items-end border-themeWhite p-6">
+            <h4
+              className={twMerge(
+                cormorantRegular.className,
+                "text-themeWhite text-4xl text-start"
+              )}
+            >
+              {title}
+            </h4>
+          </div>
         </div>
       </div>
-    </div>
+    </button>
   );
 };
--- toan-cau-craft/src/screens/home/MainInfoCard/index.tsx
+++ toan-cau-craft/src/screens/home/MainInfoCard/index.tsx
@@ -1,53 +1,76 @@
+"use client";
 import { cormorantRegular, latoRegular } from "@/fonts";
-import Image from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
 import { twMerge } from "tailwind-merge";
 
 export const MainInfoCard = (): React.JSX.Element => {
   return (
-    <div className="grid grid-cols-5 bg-themeWhite rounded-3xl overflow-hidden shadow-lg w-2/3">
-      <div className="col-span-2 bg-[url('/images/home-demo.png')]">
-      </div>
-      <div className="col-span-3 bg-themeWhite">
-        <div className="m-11">
-          <h1 className="text-textPrimary font-island-moments text-6xl">
-            About us
-          </h1>
-          <h2
-            className={twMerge(
-              cormorantRegular.className,
-              "text-textPrimary text-6xl"
-            )}
-          >
-            Handicraft
-          </h2>
-          <p
-            className={twMerge(
-              latoRegular.className,
-              "text-textPrimary mt-6 text-wrap"
-            )}
-          >
-            Lorem ipsum dolor sit amet consectetur. Tempor faucibus sit iaculis
-            arcu felis. Volutpat sollicitudin tortor aliquam maecenas porttitor
-            ac et blandit. Pretium urna at ac purus aliquet mauris. Sit feugiat
-            mattis turpis congue justo.
-          </p>
-          <div className="w-full flex flex-row justify-between mt-10">
-            <NumberItem keyw="products" value="123.5k+" />
-            <NumberItem keyw="years experience" value="20" />
-            <NumberItem keyw="user returned" value="97%" />
+    <AnimatePresence>
+      <div className="grid grid-cols-5 bg-themeWhite rounded-3xl overflow-hidden shadow-lg w-2/3">
+        <div className="col-span-2 bg-[url('/images/home-demo.png')]"></div>
+        <div className="col-span-3 bg-themeWhite">
+          <div className="m-11">
+            <motion.h1
+              initial={{ y: 25, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.15 }}
+              className="text-textPrimary font-island-moments text-6xl"
+            >
+              About us
+            </motion.h1>
+            <motion.h2
+              initial={{ y: 25, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              className={twMerge(
+                cormorantRegular.className,
+                "text-textPrimary text-6xl"
+              )}
+            >
+              Handicraft
+            </motion.h2>
+            <motion.p
+              initial={{ y: 25, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1 }}
+              className={twMerge(
+                latoRegular.className,
+                "text-textPrimary mt-6 text-wrap"
+              )}
+            >
+              Lorem ipsum dolor sit amet consectetur. Tempor faucibus sit
+              iaculis arcu felis. Volutpat sollicitudin tortor aliquam maecenas
+              porttitor ac et blandit. Pretium urna at ac purus aliquet mauris.
+              Sit feugiat mattis turpis congue justo.
+            </motion.p>
+            <motion.div
+              initial={{ y: 25, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1.5 }}
+              className="w-full flex flex-row justify-between mt-10"
+            >
+              <NumberItem keyw="products" value="123.5k+" />
+              <NumberItem keyw="years experience" value="20" />
+              <NumberItem keyw="user returned" value="97%" />
+            </motion.div>
+            <Link
+              className="text-textSecondary flex flex-row items-center mt-10"
+              href={"#"}
+              title="Readmore"
+            >
+              <div
+                style={{ height: 1 }}
+                className="bg-textSecondary w-10 mr-1"
+              ></div>
+              <p className={twMerge(latoRegular.className, "text-base")}>
+                READ MORE
+              </p>
+            </Link>
           </div>
-          <Link
-            className="text-textSecondary flex flex-row items-center mt-10"
-            href={"#"}
-            title="Readmore"
-          >
-            <div style={{ height: 1 }} className="bg-textSecondary w-10 mr-1"></div>
-            <p className={twMerge(latoRegular.className, "text-base")}>READ MORE</p>
-          </Link>
         </div>
       </div>
-    </div>
+    </AnimatePresence>
   );
 };
 
@@ -63,7 +86,10 @@ const NumberItem = ({ keyw, value }: { keyw: string; value: string }) => {
         {value}
       </p>
       <p
-        className={twMerge(latoRegular.className, "text-textSecondary text-base mt-2")}
+        className={twMerge(
+          latoRegular.className,
+          "text-textSecondary text-base mt-2"
+        )}
       >
         {keyw}
       </p>
