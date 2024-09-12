76f4feb4d3728dbd8b620ce9b5cceb25e9651b99
Lai Ngoc Lam <laingoclam3112@gmail.com>
2024-09-13 00:57:03 +0700

    fix bug build

--- toan-cau-craft/src/app/products/page.tsx
+++ toan-cau-craft/src/app/products/page.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { MainLayout } from "@/components";
 import { Products } from "@/screens";
 export default function ProductsPage() {
   return (
     <main>
       <MainLayout>
-        <Products />
+        <Suspense>
+          <Products />
+        </Suspense>
       </MainLayout>
     </main>
   );
